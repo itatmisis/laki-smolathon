35016d3636a64a046c89d68a04b3d523002a1dd1
Nekiy <Nek_iy@mail.ru>
2023-11-11 00:38:48 +0300

    add new routes for category

--- app/src/services/map_service.py
+++ app/src/services/map_service.py
@@ -28,13 +28,18 @@ async def create_location_list(location_list: list[NewLocation]):
         await db.create_location_list(session, location_list)
 
 
-async def create_category_list(category_list: list[NewCategory]):
-    async with async_session() as session:
-        await db.create_category_list(session, category_list)
-
-
 async def visit_location(user_id: int, location_id: int):
     async with async_session() as session:
         await db.visit_location(session, user_id, location_id)
 
 
+async def get_all_category():
+    all_category = []
+    async with async_session() as session:
+        all_category = await db.get_all_category(session)
+    return all_category
+
+
+async def create_category_list(category_list: list[NewCategory]):
+    async with async_session() as session:
+        await db.create_category_list(session, category_list)
