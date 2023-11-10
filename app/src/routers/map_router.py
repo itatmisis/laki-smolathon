2235600a6361bbde78962167b27b98208029bbaf
Nekiy <Nek_iy@mail.ru>
2023-11-10 23:28:20 +0300

    unblock location route

--- app/src/routers/map_router.py
+++ app/src/routers/map_router.py
@@ -16,27 +16,25 @@ map_router = APIRouter(
 )
 
 @map_router.get('/location')
-async def route_get_all_locations(current_user: UserOut = Depends(get_current_user)):
+async def route_get_all_locations():
     all_location = await get_all_locations()
     return all_location
 
 
-
 @map_router.get('/location/{location_id}', response_model=GetLocation)
-async def route_get_location_by_id(location_id: int, current_user: UserOut = Depends(get_current_user)):
+async def route_get_location_by_id(location_id: int):
     location = await get_location_by_id(location_id)
     return location
 
 
 @map_router.post('/location')
-async def route_create_location_list(location_list: list[NewLocation], current_user: UserOut = Depends(get_current_user)):
+async def route_create_location_list(location_list: list[NewLocation]):
     await create_location_list(location_list)
 
 
 @map_router.post('/category')
-async def route_create_category_list(category_list: list[NewCategory], current_user: UserOut = Depends(get_current_user)):
+async def route_create_category_list(category_list: list[NewCategory]):
     await create_category_list(category_list)
-
 
 
 @map_router.post('/location/{location_id}')
