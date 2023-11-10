b9935261f3c84582ebd2e0177bc7969176c64c7a
ClayenKitten <dima070704@yandex.ru>
2023-11-10 19:07:39 +0300

    Use styled components for journal stats overview

---
+++ front/src/routes/journal/StatEntry.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import { CategoriesIcons, type Category } from "$lib/core/places";
+
+    export let category: Category;
+    export let count: number;
+
+    $: _category = CategoriesIcons[category];
+</script>
+
+<span style:--background={_category.color}>
+    <mark>{count}</mark>
+    {category}
+</span>
+
+<style lang="scss">
+    span {
+        display: flex;
+        align-items: center;
+        gap: 4px;
+        color: var(--white2);
+        padding: 8px;
+        border-radius: 8px;
+        font-size: 13px;
+        background-color: var(--background);
+        > mark {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 16px;
+            height: 16px;
+            text-align: center;
+            color: var(--background);
+            background-color: var(--white2);
+            border-radius: 2px;
+        }
+    }
+</style>
--- front/src/routes/journal/Statistics.svelte
+++ front/src/routes/journal/Statistics.svelte
@@ -1,21 +1,17 @@
+<script lang="ts">
+    import StatEntry from "./StatEntry.svelte";
+</script>
+
 <div>
     <p class="overall">
-        Вы посетили <mark>88</mark> Смоленских место<br />
+        Вы посетили <mark>88</mark> Смоленских мест<br />
         Заполнили дневник для <mark>12</mark> из них<br />
     </p>
     <div class="categories">
-        <span>
-            <mark>5</mark>
-            Соборов
-        </span>
-        <span>
-            <mark>5</mark>
-            Музеев
-        </span>
-        <span>
-            <mark>5</mark>
-            Памятников
-        </span>
+        <StatEntry category="Памятники" count={5} />
+        <StatEntry category="История" count={5} />
+        <StatEntry category="Церкви" count={5} />
+        <StatEntry category="Музеи" count={5} />
     </div>
 </div>
 
@@ -23,7 +19,7 @@
     .overall {
         font-size: 15px;
         > mark {
-            background-color: #d9d9d9;
+            background-color: var(--white1);
             padding: 2px 8px;
             border-radius: 8px;
         }
@@ -31,28 +27,7 @@
     }
     .categories {
         display: flex;
+        flex-wrap: wrap;
         gap: 4px;
-        > span {
-            display: flex;
-            align-items: center;
-            gap: 4px;
-            background-color: #727272;
-            color: white;
-            padding: 8px;
-            border-radius: 8px;
-            font-size: 13px;
-
-            > mark {
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                width: 16px;
-                height: 16px;
-                text-align: center;
-                color: #727272;
-                background-color: #d9d9d9;
-                border-radius: 2px;
-            }
-        }
     }
 </style>
